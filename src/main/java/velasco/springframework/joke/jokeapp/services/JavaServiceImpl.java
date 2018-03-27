cda193d56a84de7fffd969962e980b2d3f09ca9e
Vinícius Velasco <sasukegc@gmail.com>
2018-03-27 14:26:11 -0300

    Added commentary in JavaServiceImpl

--- src/main/java/velasco/springframework/joke/jokeapp/services/JavaServiceImpl.java
+++ src/main/java/velasco/springframework/joke/jokeapp/services/JavaServiceImpl.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class JavaServiceImpl implements JokeService {
 
-    private final ChuckNorrisQuotes chuckNorrisQuotes;
+    private final ChuckNorrisQuotes chuckNorrisQuotes; //https://mvnrepository.com/artifact/guru.springframework/chuck-norris-for-actuator/0.0.2
 
     public JavaServiceImpl() {
         this.chuckNorrisQuotes = new ChuckNorrisQuotes();
